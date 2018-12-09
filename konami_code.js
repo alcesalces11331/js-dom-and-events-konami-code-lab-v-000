89009ce49df30b63e2f6ca66c85741242dc439c3
alcesalces11331 <rollin.metzger@gmail.com>
2018-12-09 15:30:44 -0800

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,19 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
 
+  let index = 0;
+  
+  window.addEventListener("keydown", function(event) {
+    const key = parseInt(event.which || event.detail);
+    
+    if (key === code[index]) {
+      index++;
+      if (index === code.length) {
+        window.alert('Hooray!');
+      }
+    } else {
+      index=0;
+    }
+  });
 }
